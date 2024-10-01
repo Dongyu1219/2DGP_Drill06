93ba2c95b67c9d8f3ffdb5a8915f79cd4fb2c196
east_star00 <1219kdk@gmail.com>
2024-10-01 17:47:06 +0900

    마우스 위치로 이동

--- 2023180009_Drill06.py
+++ 2023180009_Drill06.py
@@ -1,18 +1,43 @@
 from pico2d import *
 
-open_canvas()
-grass = load_image('TUK_GROUND.png')
-character = load_image('run_animation.png')
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
+TUK_ground = load_image('TUK_GROUND.png')
+character = load_image('animation_sheet.png')
+
+
+def handle_events():
+    global running
+    global x, y
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_MOUSEMOTION:
+            x, y = event.x, TUK_HEIGHT - 1 - event.y
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+    pass
+
+
+
+running = True
+x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
+hide_cursor()
 
-for x in range(0, 800, 10):
- clear_canvas()
- grass.draw(800 // 2, 600 // 2, 800, 600)
- character.clip_draw(frame * 100, 0, 100, 100, x, 90)
- update_canvas()
- frame = (frame + 1) % 8
- delay(0.05)
+while running:
+    clear_canvas()
+    TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    update_canvas()
+    frame = (frame + 1) % 8
 
+    handle_events()
 
 close_canvas()
+
+
+
+
