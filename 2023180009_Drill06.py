ef689ffa8f1b3296230ec6e9388c55a08e32955f
east_star00 <1219kdk@gmail.com>
2024-10-04 01:52:51 +0900

    커서 이동 구현

--- 2023180009_Drill06.py
+++ 2023180009_Drill06.py
@@ -11,39 +11,56 @@ curser = load_image('hand_arrow.png')
 
 def handle_events():
     global running
-    global x, y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
 
-def move_character(x, y, targetX, targetY):
-    x_distance = targetX-x
-    y_distance = targetY-y
-    a = y_distance / x_distance
-    b = y -x*a
+def make_random():
+    global  curserX, curserY
+    curserX, curserY = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)
 
 running = True
 arrive = True
 
-x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
-curserX, curserY =  random.randint(1, TUK_WIDTH), random.randint(1, TUK_HEIGHT)
+x, y = TUK_WIDTH//2, TUK_HEIGHT//2
+curserX, curserY = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)
 frame = 0
-hide_cursor()
+dir = 0
+dir2 = 0
+
+arrive = False
 
 while running:
+    dir_x = curserX - x
+    dir_y = curserY - y
     clear_canvas()
+    if curserX> x:
+        dir = 1
+    elif curserX < x :
+        dir = -1
+    if curserY > y:
+        dir2 = 1
+    elif curserY <y:
+        dir2 = -1
+
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    curser.draw(curserX, curserY)
+    if arrive == False:
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    elif arrive == True:
+        make_random()
     update_canvas()
     frame = (frame + 1) % 8
+    if dir_x !=0 or dir_y != 0:
+        x += dir * 5
+        y += dir2 * 5
+    else:
+        arrive = True
     delay(0.05)
-
     handle_events()
 
 close_canvas()
